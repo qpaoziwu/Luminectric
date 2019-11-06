0375c55a9fa6ef6e67c0bfe257db0723feb70e0c
Richard <richrdhw@gmail.com>
2019-11-06 18:33:49 -0500

    Setup for custom menu

---
+++ Assets/Scripts/Editor/AttachableScriptableDataInspector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(AttachableScriptableData))]
+public class AttachableScriptableDataInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SerializedProperty itemType = serializedObject.FindProperty("itemType");
+        SerializedProperty itemName = serializedObject.FindProperty("itemName");
+        SerializedProperty attachValue = serializedObject.FindProperty("attachValue");
+
+        EditorGUILayout.PropertyField(itemType);
+        EditorGUILayout.PropertyField(itemName);
+        EditorGUILayout.PropertyField(attachValue, true);
+
+        if (itemType.enumValueIndex == 0)
+        {
+            SerializedProperty source = serializedObject.FindProperty("source");
+            EditorGUILayout.PropertyField(source, true);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/Scripts/Item Data/AttachableScriptableData.cs
+++ Assets/Scripts/Item Data/AttachableScriptableData.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Attachable/Default Item")]
 public class AttachableScriptableData : ScriptableObject
 {
-    [System.Serializable]
     public enum ItemType
     {
         Source,
@@ -17,7 +17,54 @@ public class AttachableScriptableData : ScriptableObject
 
     public ItemType itemType;
     public string itemName;
-    [Range(0,1)]
+    [Range(0, 1)]
     public int[] attachValue = new int[6];
 
+    public SourceType source = new SourceType();
+
+    [Serializable]
+    public class SourceType 
+    {
+        public string elementType;
+    }
+
+
+
+    public SourceType medium = new SourceType();
+
+    [Serializable]
+    public class OutputType
+    {
+        public string elementType;
+    }
+
+
+    public SourceType output = new SourceType();
+
+    [Serializable]
+    public class SourceType
+    {
+        public string elementType;
+    }
+
+
+
+    public SourceType shell = new SourceType();
+
+    [Serializable]
+    public class SourceType
+    {
+        public string elementType;
+    }
+
+
+
+    public SourceType source = new SourceType();
+
+    [Serializable]
+    public class SourceType
+    {
+        public string elementType;
+    }
 }
+
